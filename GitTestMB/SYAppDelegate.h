6ae32a362cee64047f98168442c99233722b0535
SunnyYear <SunnyYear@outlook.com>
2014-04-30 20:15:17 +0200

    Initial Commit

---
+++ GitTestMB/SYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SYAppDelegate.h
+//  GitTestMB
+//
+//  Created by Marco on 30/04/14.
+//  Copyright (c) 2014 SunnyYear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTestMBTests/GitTestMBTests.m
@@ -0,0 +1,34 @@
+//
+//  GitTestMBTests.m
+//  GitTestMBTests
+//
+//  Created by Marco on 30/04/14.
+//  Copyright (c) 2014 SunnyYear. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GitTestMBTests : XCTestCase
+
+@end
+
+@implementation GitTestMBTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
