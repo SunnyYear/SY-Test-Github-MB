f4bd74e43aac10c2ca3a0c53f43b4a5f6c7f734d
SunnyYear <SunnyYear@outlook.com>
2014-04-30 20:23:41 +0200

    adding a new class

---
+++ GitTestMB/SYAddNewClass.h
@@ -0,0 +1,13 @@
+//
+//  SYAddNewClass.h
+//  GitTestMB
+//
+//  Created by Marco on 30/04/14.
+//  Copyright (c) 2014 SunnyYear. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SYAddNewClass : NSObject
+
+@end
---
+++ GitTestMB/SYAddNewClass.m
@@ -0,0 +1,13 @@
+//
+//  SYAddNewClass.m
+//  GitTestMB
+//
+//  Created by Marco on 30/04/14.
+//  Copyright (c) 2014 SunnyYear. All rights reserved.
+//
+
+#import "SYAddNewClass.h"
+
+@implementation SYAddNewClass
+
+@end
